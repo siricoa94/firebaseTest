1a9f915b8ade6b16e9c865a122c06c4e9f166483
unknown <12siricoa94@gmail.com>
2019-10-31 02:38:09 -0400

    solved some issues, able to see that user is signed in, but still can not create user on page itself

--- firebase.js
+++ firebase.js
@@ -1,36 +1,34 @@
-  // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyBzypdC3VeT_pvzX7KAwbxIAA4S_oZ7DiY",
-    authDomain: "bank-of-andrew.firebaseapp.com",
-    databaseURL: "https://bank-of-andrew.firebaseio.com",
-    projectId: "bank-of-andrew",
-    storageBucket: "bank-of-andrew.appspot.com",
-    messagingSenderId: "546770562236",
-    appId: "1:546770562236:web:dd7848ed4c0e03dfa434b9",
-    measurementId: "G-XFSNBP5MJB"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
-var emailText = document.getElementById("exampleInputEmail1");
-var passwordText = document.getElementById("exampleInputPassword1");
+firebase.auth().onAuthStateChanged( user => {
+  if (user) {
+    console.log("user signed in");
+  } else {
+    console.log("no user signed in");
+  }
+});
+var txtEmail = document.getElementById("exampleInputEmail1");
+var txtPassword = document.getElementById("exampleInputPassword1");
 var logIn = document.getElementById("logIn");
 var logOut = document.getElementById("logOut");
 var signUp = document.getElementById("signUp");
 
-logIn.addEventListener("click", e => {
-var email = emailText.value;
-var password = passwordText.value;
-var auth = firebase.auth();
-var promise = auth.signInWithEmailAndPassword(email, password);
-promise.catch(e => console.log(e.message));
-console.log("hit");
-});
 signUp.addEventListener("click", e => {
-var email = emailText.value;
-var password = passwordText.value;
-var auth = firebase.auth();
-var promise = auth.createUserWithEmailAndPassword(email, password);
-promise.catch(e => console.log(e.message));
+  var email = txtEmail.value;
+  var password = txtPassword.value;
+  var auth = firebase.auth();
+  var promise = auth.createUserWithEmailAndPassword(email, password);
+  promise
+  .catch(e => console.log(e.message));
+  alert(email);
+});
+logOut.addEventListener("click", e => {
+  firebase.auth().signOut();
+});
+logIn.addEventListener("click", e => {
+  var email = txtEmail.value;
+  var password = txtPassword.value;
+  var auth = firebase.auth();
+  var promise = auth.signInWithEmailAndPassword(email, password);
+  promise
+  .catch(e => console.log(e.message));
+  e.preventDefault();
 });
